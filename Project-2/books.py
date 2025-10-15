cd43371af9c7f8b4be1a2caa6929f5317e7665fd
Khoa Nguyen <nguyendangkhoa211@gmail.com>
2025-10-15 14:47:36 +0800

    84 BOOK class and BOOKS list initialization

--- Project-2/books.py
+++ Project-2/books.py
@@ -4,10 +4,30 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+class Book:
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: int
+
+    def __init__(self, id, title, author, description, rating):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.description = description
+        self.rating = rating
 
 
 
-BOOKS = []
+BOOKS = [
+    Book(1, 'Computer Science Pro', 'codingwithroby', 'A very nice book!', 5),
+    Book(2, 'Be Fast with FastAPI', 'codingwithroby', 'A great book!', 5),
+    Book(3, 'Master Endpoints', 'codingwithroby', 'A awesome book!', 5),
+    Book(4, 'HP1', 'Author 1', 'Book Description', 2),
+    Book(5, 'HP2', 'Author 2', 'Book Description', 3),
+    Book(6, 'HP3', 'Author 3', 'Book Description', 1)
+]
 
 
 
