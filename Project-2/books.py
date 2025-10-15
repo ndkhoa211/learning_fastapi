ac18a15af7bebbb85658dfc16c9715b11cf03faf
Khoa Nguyen <nguyendangkhoa211@gmail.com>
2025-10-15 16:51:25 +0800

    99 add 404 Not Found status code using HTTPExecption to endpoints where the requested items (using book ID) doesn't exist

--- Project-2/books.py
+++ Project-2/books.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path, Query
+from fastapi import FastAPI, Path, Query, HTTPException
 from pydantic import BaseModel, Field
 from typing import Optional
 
@@ -64,10 +64,11 @@ async def read_book(book_id: int = Path(gt=0)):
     for book in BOOKS:
         if book.id == book_id:
             return book
+    raise HTTPException(status_code=404, detail='Item not found')
 
 
 @app.get("/books/")
-async def read_book_by_rating(book_rating: int Query(gt=0, lt=6)):
+async def read_book_by_rating(book_rating: int = Query(gt=0, lt=6)):
     books_to_return = []
     for book in BOOKS:
         if book.rating == book_rating:
@@ -98,14 +99,22 @@ def find_book_id(book: Book): # create ID for a new created book from /create-bo
 
 @app.put("/books/update_book")
 async def update_book(book: BookRequest):
+    book_changed = False
     for i in range(len(BOOKS)):
         if BOOKS[i].id == book.id:
             BOOKS[i] = book
+            book_changed = True
+    if not book_changed:
+        raise HTTPException(status_code=404, detail='Item not found')
 
 
 @app.delete("/books/{book_id}")
 async def delete_book(book_id: int = Path(gt=0)):
+    book_changed = False
     for i in range(len(BOOKS)):
         if BOOKS[i].id == book_id:
             BOOKS.pop(i)
+            book_changed = True
             break
+    if not book_changed:
+        raise HTTPException(status_code=404, detail='Item not found')
