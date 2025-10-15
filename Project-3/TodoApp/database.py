4dcdda8669583b6569313f2efaf51b625bfcbecb
Khoa Nguyen <nguyendangkhoa211@gmail.com>
2025-10-15 17:49:07 +0800

    104 SQLite database setup with SQLAlchemy

---
+++ Project-3/TodoApp/database.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+# create a location of db on FastAPI app
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./TodoApp/todosapp.db'
+
+# database engine is used to open up a connection and is able to use database
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, 
+    connect_args={'check_same_thread': False} # to allow multiple threads to access the database
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
