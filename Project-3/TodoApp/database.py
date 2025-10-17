db47893e4fbccfba65351c58a4f3b2fe3f7bb79b
Khoa Nguyen <nguyendangkhoa211@gmail.com>
2025-10-18 00:30:19 +0800

    143 connect FastAPI to PostgreSQL

--- Project-3/TodoApp/database.py
+++ Project-3/TodoApp/database.py
@@ -3,13 +3,11 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # create a location of db on FastAPI app
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+# SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:khoataigioi@localhost/TodoApplicationDatabase'
 
 # database engine is used to open up a connection and is able to use database
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, 
-    connect_args={'check_same_thread': False} # to allow multiple threads to access the database
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
